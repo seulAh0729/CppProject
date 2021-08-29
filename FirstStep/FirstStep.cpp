40c3f06889f5eaa60f9b993356888b0e3cd03324
DESKTOP-3OGR79T\seulAh <seulAh0729@github.com>
2021-08-29 19:29:04 +0900

    2021-08-29 c++ 수업

--- FirstStep/FirstStep.cpp
+++ FirstStep/FirstStep.cpp
@@ -1,7 +1,13 @@
 #include	<iostream>
 
+using namespace std;
+
 int main() {
 
-	std::cout << "나의 첫 번째 c++ 프로그램" << std::endl;
+	// 표준 출력 스트림으로 문장을 출력함
+	cout << "나의 첫 번째 c++ 프로그램"
+		<< endl;
+
 	return 0;
+
 }
